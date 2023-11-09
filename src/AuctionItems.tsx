62e4ce31e0c7375c765309cc7973e17558d19cfd
nived-cv <nived.cv@vonnue.com>
2023-11-09 18:36:09 +0530

    almost complete

--- src/AuctionItems.tsx
+++ src/AuctionItems.tsx
@@ -1,55 +1,68 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { AuctionItem } from "./AuctionItem";
 import './css/auction-items-list.css'
 
-let auctionItems = {
-    1:{"id": 1,"name" : "vessel" , "start_bid": 1000,
-     "current_bid":1000, "bid_person":""},
-     2:{"id": 2,"name" : "Pot" , "start_bid": 2000,
-     "current_bid":2000, "bid_person":""},
-}
+let auctionItems = [
+    { "id": 1, "name": "vessel", "start_bid": 1000, "current_bid": 1000, "bid_person": "" },
+    { "id": 2, "name": "Pot", "start_bid": 2000, "current_bid": 2000, "bid_person": "" },
+]
 
 type mapArgs = {
 
-    id : number
-    name : string
-    start_bid : number
-    current_bid : number
-    bid_person : string 
+    id: number
+    name: string
+    start_bid: number
+    current_bid: number
+    bid_person: string
 }
 
-export const AuctionItems = () =>{
+type propstype = {
+    props : {id:number,name:string}
+}
+
+//const getBid = (item : mapArgs) =>  item.current_bid
+
+const submitBid = (item: mapArgs, amount: number, person: string, state: mapArgs[],
+    setstate: React.Dispatch<React.SetStateAction<mapArgs[]>>) => {
+
+    item.bid_person = person
+    item.current_bid = amount
+    let new_arr = [...state]
+    setstate(new_arr)
+}
+
+export const AuctionItems = ({props}:propstype) => {
 
     let [itemDetails, setItemDetails] = useState(auctionItems)
+    let [bidAmount,setBidAmount] = useState<null | number>(null)
+    const amountObj = useRef<HTMLInputElement>(null)
 
-    const selected = (selectedItem :mapArgs) =>{
+    const selected = (selectedItem: mapArgs) => {
 
-            //let id = selectedItem.id
-            // setItemDetails(()=>{
-            //     let temp = itemDetails.1
-            // })
-           //     selectedItem.current_bid += 10
-           
+        setBidAmount(selectedItem.current_bid)
+        if (amountObj.current){
 
-           //let prev =  Object.assign({},itemDetails)
-           selectedItem.current_bid +=1
-        //    let updated = {selectedItem.id : }
-        //    Objects.assign
-            let newDuplicate = Object.assign({},itemDetails)
-
-           setItemDetails(newDuplicate)
-           console.log(selectedItem,itemDetails)
-
+            let value = Number(amountObj.current.value)
+            if( value > selectedItem.current_bid)
+                submitBid(selectedItem, value,props.name, itemDetails, setItemDetails)
+            
+            amountObj.current.value = ""
+        }
     }
 
-    return(
-        <div className = "auction-items-list">
+    return (
+        <div className = "auction-items-container">
+            <div className="auction-items-list">
 
-            {Object.values(itemDetails).map((item : mapArgs) => {
-                return <AuctionItem key = {item.id} selected = {selected} item = {item}/>
-            })}
+                {itemDetails.map((item: mapArgs) => {
+                    return <AuctionItem key={item.id} selected={selected} item={item} />
+                })}
+
+            </div>
             
+            <div className = "bid-form">
+                <input type = "number" min={bidAmount!} ref = {amountObj}/>
+            </div>
         </div>
     )
 }
-// 
--- src/ContentScreen.tsx
+++ src/ContentScreen.tsx
@@ -1,10 +1,25 @@
 import { AuctionItems } from "./AuctionItems"
+import { AuctionUsers } from "./AuctionUsers"
+import { useState } from 'react'
+
+type user = {
+    id : number
+    name : string
+}
 
 export const ContentScreen = () =>{
 
+    const [user,setUser] = useState <user> ({} as user)
+    
+    const passCurrentUser = (user : user) =>{
+
+        setUser(user)
+    }
+
     return (
     <div>
-        <AuctionItems />
+        <AuctionItems props = {user}/>
+        <AuctionUsers passCurrentUser = {passCurrentUser}/>
 
     </div>
     )
